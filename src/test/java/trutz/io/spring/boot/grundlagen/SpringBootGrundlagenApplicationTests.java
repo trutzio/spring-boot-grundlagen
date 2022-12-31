7978ed370751016b91c65260b0718b4306e38a75
Christian Trutz <info@trutz-software-consulting.de>
2022-12-31 15:57:44 +0100

    chore: Spring Boot Project from Scratch erzeugt

---
+++ src/test/java/trutz/io/spring/boot/grundlagen/SpringBootGrundlagenApplicationTests.java
@@ -0,0 +1,13 @@
+package trutz.io.spring.boot.grundlagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrundlagenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
