4f17e5026b1ce38ee87f03dab8af87ef5a02de2d
Christian Trutz <info@trutz-software-consulting.de>
2023-02-20 11:15:49 +0100

    fix: @NonNull Annotation für den CelsiusService

--- src/main/java/trutz/io/spring/boot/grundlagen/CelsiusService.java
+++ src/main/java/trutz/io/spring/boot/grundlagen/CelsiusService.java
@@ -1,12 +1,14 @@
 package trutz.io.spring.boot.grundlagen;
 
+
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CelsiusService {
-	
-	public double toFahrenheit(double celsius) {
-		return (celsius * 9.0)/5.0 + 32.0;
-	} 
-	
+
+	public double toFahrenheit(@NonNull Double celsius) {
+		return (celsius * 9.0) / 5.0 + 32.0;
+	}
+
 }
