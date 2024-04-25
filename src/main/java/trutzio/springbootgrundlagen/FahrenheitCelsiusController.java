bcb1b2d1f12215d8664425bc9e6ae76930ff5e16
Christian Trutz <info@trutz-software-consulting.de>
2024-04-25 14:12:35 +0200

    feat: Fahrenheit Controller

---
+++ src/main/java/trutzio/springbootgrundlagen/FahrenheitCelsiusController.java
@@ -0,0 +1,24 @@
+package trutzio.springbootgrundlagen;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class FahrenheitCelsiusController {
+
+    /**
+     * Umrechnunsgsformel °F = °C * 1.8 + 32
+     * 
+     * @param celsius
+     * @return
+     */
+    @GetMapping("to_farenhenheit")
+    public String toFahrenheit(@RequestParam Double celsius) {
+        double fahrenheit = celsius.doubleValue() * 1.8 + 32;
+        return celsius + " °C sind " + fahrenheit + " °F";
+    }
+    
+
+}
