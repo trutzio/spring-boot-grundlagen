87412cb3c0c598af31dd8afa999fa6317b913fd5
Christian Trutz <info@trutz-software-consulting.de>
2024-05-21 20:30:37 +0200

    RC1 Ende Iteration1

--- src/test/java/com/example/demo/TemperatureServiceTest.java
+++ src/test/java/com/example/demo/TemperatureServiceTest.java
@@ -1,18 +1,33 @@
 package com.example.demo;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TemperatureServiceTest {
 
-    @Test
-    void test_37C(@Autowired TemperatureService temperatureService) {
-        double computedFahrenheit = temperatureService.toFahrenheit(37.0d);
-        double expectedFahrenheit = 98.6d;
-        Assertions.assertEquals(expectedFahrenheit, computedFahrenheit, 0.001);
+    @Autowired
+    private TemperatureService temperatureService;
+
+    @ParameterizedTest
+    @MethodSource("celsiusFahrenheitSource")
+    void test_toFahrenheit(double celsius, double expectedFahrenheit) {
+        double computedFahrenheit = temperatureService.toFahrenheit(celsius);
+        Assertions.assertEquals(expectedFahrenheit, computedFahrenheit, 0.001d);
+    }
+
+    private static Stream<Arguments> celsiusFahrenheitSource() {
+        return Stream.of(
+            Arguments.of(37.0d, 98.6d),
+            Arguments.of(0.0d, 32.0d)
+        );
     }
 
 }
