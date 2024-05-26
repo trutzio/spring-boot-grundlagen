6c4af86b8c219adbc840c0589e2212b0a5aa38cf
Christian Trutz <info@trutz-software-consulting.de>
2024-05-26 20:31:38 +0200

    Graphana und Tracing eingeschaltet

--- src/main/java/com/example/demo/DataSourceSandbox.java
+++ src/main/java/com/example/demo/DataSourceSandbox.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,15 +17,20 @@ public class DataSourceSandbox implements ApplicationRunner {
 
     private final static Logger log = LoggerFactory.getLogger(DataSourceSandbox.class);
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
-    public DataSourceSandbox(DataSource dataSource) {
+    private final JdbcTemplate jdbcTemplate;
+
+    public DataSourceSandbox(DataSource dataSource, JdbcTemplate jdbcTemplate) {
         this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info(dataSource.toString());
+
+        // JDBC pure
         Connection connection = dataSource.getConnection();
         ResultSet resultSet = connection.createStatement().executeQuery("SELECT id,name FROM Person");
         while (resultSet.next()) {
@@ -32,6 +38,10 @@ public class DataSourceSandbox implements ApplicationRunner {
                     resultSet.getInt("id"),
                     resultSet.getString("name"));
         }
+
+        // JDBC Template
+        String name = jdbcTemplate.queryForObject("SELECT name FROM Person where id=1", String.class);
+        log.info("Name for 1: {}", name);
     }
 
 }
